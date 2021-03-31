a1c7a76c995ef2fb3795063c6f66eed57f8e38a0
gcem <34423323+gcem@users.noreply.github.com>
2021-03-31 23:14:19 +0300

    start creating the project structure by following cmake tutorial

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "config.h"
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cout << argv[0] << " version " << pathTracer_VERSION_MAJOR << "."
+                  << pathTracer_VERSION_MINOR << std::endl;
+        exit(1);
+    }
+    return 0;
+}
