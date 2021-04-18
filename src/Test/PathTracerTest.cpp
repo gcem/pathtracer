e4dbf313cee36529e8dc293061437ab2628e7c85
gcem <34423323+gcem@users.noreply.github.com>
2021-04-18 10:15:31 +0300

    fix a unit test broken after noise filtering

--- src/Test/PathTracerTest.cpp
+++ src/Test/PathTracerTest.cpp
@@ -10,13 +10,14 @@ class PathTracerTest
 
 TEST_F(PathTracerTest, TimeImageNormalization)
 {
-    times = { { 20, 40, 50 }, { 60, 100, 55 } };
+    // put 90 next to 100 to avoid noise filtering
+    times = { { 20, 40, 50 }, { 60, 100, 90 } };
     auto result = createTimeImage();
     EXPECT_EQ(Image::Image<unsigned char>::PixelT(51, 0, 0),
               result.getPixel(0, 0));
     EXPECT_EQ(Image::Image<unsigned char>::PixelT(102, 0, 0),
               result.getPixel(1, 0));
-    EXPECT_EQ(Image::Image<unsigned char>::PixelT(140, 0, 0),
+    EXPECT_EQ(Image::Image<unsigned char>::PixelT(229, 0, 0),
               result.getPixel(2, 1)); // 140 = 55 / 100 * 255
     EXPECT_EQ(Image::Image<unsigned char>::PixelT(255, 0, 0),
               result.getPixel(1, 1));
